3824a97933d0beb2aa46df4265ec0cbe79260809
Yunjoo Cho <yunjcho@cx1r3s2.42seoul.kr>
2023-02-14 22:24:46 +0900

    2023/02/14 BackUp

--- push_swap_bonus.c
+++ push_swap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 20:05:36 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/02/14 22:20:22 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/02/14 22:24:04 by yunjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,17 @@ int	main(int ac, char **av)
 {
 	t_stack		stack_a;
 	t_stack		stack_b;
+	t_elem		**order_arr;
 	char		*command;
 	
+	order_arr = NULL;
 	command = 0;
 	init_deque(&stack_a);
 	init_deque(&stack_b);
 	if (!is_correct_arg(ac))
 		print_exit(1);
 	parsing_argv(av, &stack_a);
+	order_arr = indexing_order(order_arr, &stack_a, ac);
 	while (1)
 	{
 		write(0, command, 3);
@@ -31,6 +34,10 @@ int	main(int ac, char **av)
 			break;
 		ps_command(command, &stack_a, &stack_b);
 	}
+	printf("stack a =============\n");
+	print_deque(&stack_a);
+	printf("stack b =============\n");
+	print_deque(&stack_b);
 	if (!check_sort(&stack_a))
 	{
 		ft_putendl_fd("KO", 1);
@@ -41,9 +48,5 @@ int	main(int ac, char **av)
 		ft_putendl_fd("OK", 1);
 		print_exit(0);
 	}
-	printf("stack a =============\n");
-	print_deque(&stack_a);
-	printf("stack b =============\n");
-	print_deque(&stack_b);
 	return (0);
 }
